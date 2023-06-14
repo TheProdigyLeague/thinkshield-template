3a8ad959a5eb99838611405da8127b8b8d9eed51
Arcronsion <duy.nguyen@thinkvitals.com>
2023-06-14 21:50:39 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,15 @@ import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  experimental: {
-    assets: true,
-  },
-  server: {
-    port: 3010,
-    host: true,
+  vite: {
+    ssr: {
+      noExternal: ["primereact", "primeicons"],
+    },
   },
   integrations: [partytown(), react(), sitemap()],
   adapter: netlify(),
---
+++ src/components/providing/PrimeReactButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PrimeReactButton = () => {
+  return (
+    <Button
+      className=" col-6  align-items-center text-green-500 hover:text-white  hover:bg-green-400 border-green-300 "
+      label="For more details"
+      text
+      rounded
+    />
+  );
+};
+
+export default PrimeReactButton;
