2e6349f30ba23917254c5a05142862725ff7a760
Arcronsion <duy.nguyen@thinkvitals.com>
2023-06-14 17:21:55 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
   experimental: {
-    assets: true,
-    hybridOutput: true,
+    assets: true
   },
   server: {
     port: 3010,
-    host: true,
+    host: true
   },
   integrations: [partytown(), react(), sitemap()],
+  adapter: vercel()
 });
